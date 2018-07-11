625757c8263c79c5cd97e45835635a2e76f375a7
sumaia2020 <sbalghaiti@hawkmail.hfcc.edu>
2018-07-11 15:24:57 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def game(number)
 
-  if number % 3
+  if number % 3 = 1
     puts "fizz"
-  elsif number % 5
+  elsif number % 5 = 1
     puts "buzz"
-  elsif number %5 && 3
+  elsif number %5 && 3 = 1
     puts "fizzbuzz"
   end 
 end 
